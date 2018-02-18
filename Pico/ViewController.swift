999c7297eb81c6692a68b61e7be73f5e6d0000dd
Robert Frank Zhang <robert@MacBook-Pro.local>
2018-02-18 09:10:02 -0500

    First push

---
+++ Pico/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pico
+//
+//  Created by Robert Frank Zhang on 2/18/18.
+//  Copyright © 2018 iLaunch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
